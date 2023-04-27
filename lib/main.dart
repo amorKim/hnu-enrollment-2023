0217025ce00785dddd9daadd515edd1e47825527
stvnsy <stevenjakeasoy>
2023-04-27 15:07:03 +0800

    Asoy Committing again

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:hnu_mis_announcement/services/auth/auth_service.dart';
 import 'package:hnu_mis_announcement/views/constants/route.dart';
 import 'package:hnu_mis_announcement/views/register_view.dart';
 import 'package:hnu_mis_announcement/views/verify_email_view.dart';
-//tesdfsfsdfdsf
+//ASOY, Steven Jake Commit
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
