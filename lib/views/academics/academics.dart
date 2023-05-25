f1f98d529b3f8d7647eb92175a8ecfb865ddabf1
amorKim <amor.kim26@gmail.com>
2023-05-25 22:44:30 +0800

    update by TIONGSON: dynamic academics

--- lib/views/academics/academics.dart
+++ lib/views/academics/academics.dart
@@ -1,109 +1,28 @@
 import 'package:flutter/material.dart';
-
-class SectionCatalog {
-  final String courseCode;
-  final String courseName;
-
-  SectionCatalog({required this.courseCode, required this.courseName});
-}
-
-class Schedule {
-  final String day;
-  final String time;
-  final String roomBuilding;
-
-  Schedule({required this.day, required this.time, required this.roomBuilding});
-}
-
-class Teacher {
-  final String name;
-
-  Teacher({required this.name});
-}
-
-class GoogleClassroom {
-  final String code;
-
-  GoogleClassroom({required this.code});
-}
-
-class Unit {
-  final String name;
-
-  Unit({required this.name});
-}
-
-class Section {
-  final SectionCatalog sectionCatalog;
-  final Schedule schedule;
-  final Teacher teacher;
-  final GoogleClassroom googleClassroom;
-  final Unit unit;
-
-  Section({
-    required this.sectionCatalog,
-    required this.schedule,
-    required this.teacher,
-    required this.googleClassroom,
-    required this.unit,
-  });
-}
-
-class Grade {
-  final double unitCredit;
-  final double preG;
-  final double midG;
-  final double finalG;
-
-  Grade({
-    required this.unitCredit,
-    required this.preG,
-    required this.midG,
-    required this.finalG,
-  });
-}
+import 'package:hnu_mis_announcement/services/auth/auth_service.dart';
+import 'package:hnu_mis_announcement/services/cloud/firebase_cloud_storage.dart';
 
 class AcademicsPage extends StatefulWidget {
+  String get userId => AuthService.firebase().currentUser!.id;
+
   const AcademicsPage({Key? key}) : super(key: key);
 
   @override
-  State<AcademicsPage> createState() => _AcademicsPageState();
+  // ignore: library_private_types_in_public_api
+  _AcademicsPageState createState() => _AcademicsPageState();
 }
 
 class _AcademicsPageState extends State<AcademicsPage> {
+  final FirebaseCloudStorage enrollmentService = FirebaseCloudStorage();
 
-  final List<Section> sections = [
-    Section(
-        sectionCatalog: SectionCatalog(courseCode: 'MATH 109', courseName: 'MATH 109'),
-        schedule: Schedule(day: 'TTh', time: '01:30PM-03:00PM', roomBuilding: '[104-B] Bates'),
-        teacher: Teacher(name: 'Arawiran, Jaremilleta'),
-        googleClassroom: GoogleClassroom(code: 'wvlrzms'),
-        unit: Unit(name: '3.0')),
-    Section(
-        sectionCatalog: SectionCatalog(courseCode: 'CCS 106', courseName: 'Application Development and Emerging Technologies'),
-        schedule: Schedule(day: 'TTh', time: '12:30PM-01:30PM\n01:30PM-03:00PM', roomBuilding: '[207-B] Bates\n[102C (Computer Lab)] Scanlon'),
-        teacher: Teacher(name: 'Amores, Thomas Andrew'),
-        googleClassroom: GoogleClassroom(code: '725t6b5'),
-        unit: Unit(name: '3.0')),
-    Section(
-        sectionCatalog: SectionCatalog(courseCode: 'SE', courseName: 'Software Engineering 2'),
-        schedule: Schedule(day: 'FSat', time: '05:30PM-06:30PM\n07:00PM-08:30PM', roomBuilding: '/[104-B] Bates\n[007-1B (ComputerLab)] Bates'),
-        teacher: Teacher(name: 'Agunod, Reynaldo'),
-        googleClassroom: GoogleClassroom(code: ''),
-        unit: Unit(name: '3.0'))
-  ];
-
-  final List<Grade> grades = [
-    Grade(unitCredit: 0.0, preG: 0.0, midG: 0.0, finalG: 0.0),
-    Grade(unitCredit: 0.0, preG: 0.0, midG: 0.0, finalG: 0.0),
-    Grade(unitCredit: 0.0, preG: 0.0, midG: 0.0, finalG: 0.0),
-  ];
-
+  late double totalGrade;
   @override
   Widget build(BuildContext context) {
+    double deviceWidth = MediaQuery.of(context).size.width;
+    double deviceHeight = MediaQuery.of(context).size.height;
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Academics',
       home: DefaultTabController(
         length: 3,
         child: Scaffold(
@@ -148,58 +67,98 @@ class _AcademicsPageState extends State<AcademicsPage> {
                   children: [
                     // Schedule Tab
                     SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: DataTable(
-                        columns: const [
-                          DataColumn(label: Text('Course Code')),
-                          DataColumn(label: Text('Course Name')),
-                          DataColumn(label: Text('Day')),
-                          DataColumn(label: Text('Time')),
-                          DataColumn(label: Text('Room / Building')),
-                          DataColumn(label: Text('Teacher')),
-                          DataColumn(label: Text('Google Classroom Code')),
-                          DataColumn(label: Text('Unit')),
-                        ],
-                        rows: [
-                          ...sections.map((section) => DataRow(cells: [
-                            DataCell(Text(section.sectionCatalog.courseCode)),
-                            DataCell(Text(section.sectionCatalog.courseName)),
-                            DataCell(Text(section.schedule.day)),
-                            DataCell(Text(section.schedule.time)),
-                            DataCell(Text(section.schedule.roomBuilding)),
-                            DataCell(Text(section.teacher.name)),
-                            DataCell(Text(section.googleClassroom.code)),
-                            DataCell(Text(section.unit.name)),
-                          ]))
-                        ],
-                      ),
-                    ),
-
-                    // Grades Tab
-                    SingleChildScrollView(
-                      child: DataTable(
-                        columns: const [
-                          DataColumn(label: Text('Credit Unit')),
-                          DataColumn(label: Text('Pre')),
-                          DataColumn(label: Text('Mid')),
-                          DataColumn(label: Text('Final')),
-                        ],
-                        rows: [
-                          ...grades.map((grades) => DataRow(
-                              cells: [
-                                DataCell(Text('${grades.unitCredit}')),
-                                DataCell(Text('${grades.preG}')),
-                                DataCell(Text('${grades.midG}')),
-                                DataCell(Text('${grades.finalG}')),
-                              ]),
+                      child: Column(
+                        children: [
+                          StreamBuilder(
+                            stream: enrollmentService.allEnrollmentsOfStudent(userId: widget.userId),
+                            builder: (context, enrollments) {
+                              if (!enrollments.hasData || enrollments.data!.isEmpty) {
+                                return const Text(
+                                  'No Enrollments yet',
+                                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                                );
+                              }
+                              // Handle the case when data is available
+                              return DataTable(
+                                dataRowHeight: deviceHeight * 0.09,
+                                headingRowHeight: deviceHeight * 0.08,
+                                columnSpacing: deviceWidth * 0.02,
+                                columns: const [
+                                  DataColumn(label: Text('Course \nName')),
+                                  DataColumn(label: Text('Day')),
+                                  DataColumn(label: Text('Room')),
+                                  DataColumn(label: Text('Time')),
+                                ],
+                                rows: [
+                                  ...?enrollments.data?.map((enrollment) => DataRow(cells: [
+                                    DataCell(Text(enrollment.courseCode)),
+                                    DataCell(Text(enrollment.schedule['days'])),
+                                    DataCell(Text(enrollment.schedule['room'])),
+                                    DataCell(Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        Text(enrollment.schedule['start_time']),
+                                        Text(enrollment.schedule['end_time']),
+                                      ],
+                                    ),
+                                    ),
+                                  ])),
+                                ],
+                              );
+                            },
                           ),
                         ],
                       ),
                     ),
-
+                    // Grades Tab
+                    SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          StreamBuilder(
+                            stream: enrollmentService.allEnrollmentsOfStudent(userId: widget.userId),
+                            builder: (context, enrollments) {
+                              if (!enrollments.hasData || enrollments.data!.isEmpty) {
+                                return const Text(
+                                  'No Enrollments yet',
+                                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                                );
+                              } else {
+                                totalGrade = enrollments.data!.toList().length * 2310.66;
+                                // Handle the case when data is available
+                                return DataTable(
+                                  dataRowHeight: deviceHeight * 0.09,
+                                  headingRowHeight: deviceHeight * 0.08,
+                                  columnSpacing: deviceWidth * 0.1,
+                                  columns: const [
+                                    DataColumn(label: Text('Credit Units')),
+                                    DataColumn(label: Text('Prelim')),
+                                    DataColumn(label: Text('Midterm')),
+                                    DataColumn(label: Text('Final')),
+                                  ],
+                                  rows: [
+                                    ...?enrollments.data?.map((enrollment) => DataRow(
+                                      cells: [
+                                        DataCell(Text(enrollment.studGrade ?? '0.0')),
+                                        DataCell(Text(enrollment.studGrade ?? '0.0')),
+                                        DataCell(Text(enrollment.studGrade ?? '0.0')),
+                                        DataCell(Text(enrollment.studGrade ?? '0.0')),
+                                      ],
+                                    )),
+                                  ],
+                                );
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
                     // Prospectus Tab
                     SingleChildScrollView(
                       child: DataTable(
+                        dataRowHeight: deviceHeight * 0.09,
+                        headingRowHeight: deviceHeight * 0.08,
+                        columnSpacing: deviceWidth * 0.1,
                         columns: const [
                           DataColumn(label: Text('Code')),
                           DataColumn(label: Text('Descriptive Title')),
@@ -210,7 +169,6 @@ class _AcademicsPageState extends State<AcademicsPage> {
                             DataCell(Text('AL 102')),
                             DataCell(Text('Automata Theory \nand Formal \nLanguages')),
                             DataCell(Text('3.0')),
-
                           ]),
                           DataRow(cells: [
                             DataCell(Text('CCS 106')),
